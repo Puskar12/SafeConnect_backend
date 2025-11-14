474f7c7c9d90ef126392694c160fe580488230b0
Puskar Das <puskardas0318@gmail.com>
2025-11-14 16:12:50 +0530

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://safe-connect-backend.vercel.app/",
   credentials: true
 }));
 
