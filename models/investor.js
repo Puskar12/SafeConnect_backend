cd3b5f65ee3a297c6fdb917f29eec29c5c222dc9
Puskar Das <puskardas0318@gmail.com>
2025-11-15 10:49:25 +0530

    first commit

--- models/investor.js
+++ models/investor.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const investorSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["investor", "hr"], default: "investor" },
+  role: { type: String, enum: ["investor", "hr", "member"], default: "investor" },
 });
 
 const Investor = mongoose.model("Investor", investorSchema);
